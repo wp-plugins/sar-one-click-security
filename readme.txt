5acd31bb3bf2675503ba8234abed6f6c25b40416
samuelaguilera <samuelaguilera@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-14 20:58:46 +0000

    First public release
    git-svn-id: https://plugins.svn.wordpress.org/sar-one-click-security/trunk@966024 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== SAR One Click Security ===
+Contributors: samuelaguilera
+Tags: security, htaccess, spam, comments, bots, registration
+Requires at least: 3.9.2
+Tested up to: 3.9.2
+Stable tag: 1.0
+License: GPL3
+
+Adds some extra security to your WordPress with only one click. No options page, just activate it!
+
+== Description ==
+
+There's a lot of WordPress security plugins with many many options and pages to setup. And that is fine if you know what to do.
+But most of the times, you don't need so much or simply you're not sure about what to set or not.
+
+This plugin adds some extra security to your WordPress with only one click. No options page, just activate it!
+
+The plugin uses well known .htaccess rules, safe to use in almost any type of site, to protect your WordPress from common attacks.
+
+It has been tested in many servers including large providers like HostGator and Godaddy with optimal results, and it will work fine in any decent hosting service (that allows you to set options from .htaccess files). Anyway, if you get any problem after activating the plugin, check FAQ for instructions on how to manually uninstall it.
+
+= Features =
+
+* Turn off ServerSignature directive, that may leak information about your web server.
+* Turn off directoy listing, avoiding bad configured hostings to leak your files.
+* Blocks access to following files, thay may leak information about your WordPress install: .htacces, license.txt, readme.html, wp-confing.php, wp-config-sample.php
+* Stops dummy bots trying to register in WordPress sites that have registration disabled.
+* Blocks requests looking for timthumb.php (this means that you can't use a theme with TimThumb and this plugin together).
+* Blocks TRACE and TRACK request methods.
+* Blocks direct posting to wp-comments-post.php (most spammers do this).
+* Blocks direct access to PHP files in wp-content directory.
+
+= Requirements =
+
+* WordPress 3.9.2 or higher.
+* Apache2 web server
+* A theme that doesn't use Timthumb script (any decent recent theme does not use it).
+
+= Usage =
+
+Simply install and activate, no options page.
+ 	
+== Installation ==
+
+* Extract the zip file and just drop the contents in the <code>wp-content/plugins/</code> directory of your WordPress installation (or install it directly from your dashboard) and then activate it from Plugins page.
+
+== Frequently Asked Questions ==
+
+= After activating the plugin I get an error 500 page, what can I do? =
+
+If you get an error 500 page after activating the plugin this means that your hosting provider doesn't allow you to set some (or any) settings from your .htaccess
+
+You can manually uninstall plugin's .htacces rules by open your favorite FTP client and removing all content between # BEGIN SAR One Click Security and # END SAR One Click Security in your .htaccess file located in the root directory of your WordPress installation.
+And doing the same in the .htaccess file located in the wp-content dir (or deleting the file if no more content on it).
+
+== Changelog ==
+
+= 1.0 =
+
+* First release.
---
+++ sar-one-click-security.php
@@ -0,0 +1,132 @@
+<?php
+/*
+Plugin Name: SAR One Click Security
+Plugin URI: http://www.samuelaguilera.com/
+Description: Adds some extra security to your WordPress with only one click. No options page, just activate it!
+Author: Samuel Aguilera
+Version: 1.0
+Author URI: http://www.samuelaguilera.com
+License: GPL3
+*/
+
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function SAR_Add_Security_Rules(){
+
+	// Path to .htaccess
+	$htaccess = get_home_path().".htaccess";
+	$wp_content_htaccess = WP_CONTENT_DIR.'/.htaccess';
+
+	// WordPress domain
+	$wp_url = get_bloginfo( 'wpurl' );
+	$wp_url = parse_url($wp_url);
+	$wp_domain = preg_replace('#^www\.(.+\.)#i', '$1', $wp_url['host']); // only removes www from beginning, allowing domains that contains www on it
+	$wp_domain = explode(".",$wp_domain);
+
+	// Support for multisite subdomains
+	$domain_parts = count($wp_domain);
+	if ($domain_parts === 2) {
+		$wp_domain_exploded = $wp_domain[0].'\.'.$wp_domain[1];
+	} elseif ($domain_parts === 3) {
+		$wp_domain_exploded = $wp_domain[0].'\.'.$wp_domain[1].'\.'.$wp_domain[2];
+	} else {
+		$wp_domain_not_supported = true; // for IP based URLs
+	}
+
+
+	// Security rules	 
+	$sec_rules = array();
+	$sec_rules[] = "# Any decent hosting should have this set, but many don't have";
+	$sec_rules[] = 'ServerSignature Off'.PHP_EOL.'IndexIgnore *'; // Options -Indexes maybe is better, but some hostings doesn't allow the use of Options directives from .htaccess
+
+	$sec_rules[] = '# Block access to sensitive files';
+	$sec_rules[] = "<Files .htaccess>".PHP_EOL."order allow,deny".PHP_EOL."deny from all".PHP_EOL."</Files>";
+	$sec_rules[] = '<FilesMatch "^(license|readme|wp-config|wp-config-sample).*$">'.PHP_EOL.'order allow,deny'.PHP_EOL.'deny from all'.PHP_EOL.'</FilesMatch>';
+
+	$sec_rules[] = '# Stops dummy bots trying to register in WordPress sites that have registration disabled';
+	$sec_rules[] = '<IfModule mod_rewrite.c>'.PHP_EOL.'RewriteEngine On';
+	$sec_rules[] = 'RewriteCond %{QUERY_STRING} (registration=disabled|action=register) [NC,OR]'.PHP_EOL.'RewriteCond %{HTTP_REFERER} registration=disabled [NC]'.PHP_EOL.'RewriteRule ^wp-login.php http://127.0.0.1 [L,R=301]';
+	$sec_rules[] = '</IfModule>';
+
+	$sec_rules[] = '# Block requests looking for timthumb.php';	
+	$sec_rules[] = '<IfModule mod_rewrite.c>'.PHP_EOL.'RewriteEngine On';
+	$sec_rules[] = 'RewriteRule ^(.*)/?timthumb\.php$ http://127.0.0.1 [L,R=301,NC,QSA]';
+	$sec_rules[] = '</IfModule>';
+
+	$sec_rules[] = '# Block TRACE and TRACK request methods';	
+	$sec_rules[] = '<IfModule mod_rewrite.c>'.PHP_EOL.'RewriteEngine On';
+    $sec_rules[] = 'RewriteCond %{REQUEST_METHOD} ^(TRACE|TRACK)';
+    $sec_rules[] = 'RewriteRule (.*) - [F]';
+	$sec_rules[] = '</IfModule>';
+
+	if (!$wp_domain_not_supported) { // We don't want to add this if the domain is not supported...
+		$sec_rules[] = '# Blocks direct posting to wp-comments-post.php';	
+		$sec_rules[] = '<IfModule mod_rewrite.c>'.PHP_EOL.'RewriteEngine On';
+		$sec_rules[] = 'RewriteCond %{REQUEST_METHOD} ^(PUT|POST|GET)$ [NC]';
+		$sec_rules[] = 'RewriteCond %{REQUEST_URI} .wp-comments-post\.php*';
+		$sec_rules[] = 'RewriteCond %{HTTP_REFERER} !^http(s)://(www\.)?'.$wp_domain_exploded.'/.*$ [OR]';
+		$sec_rules[] = 'RewriteCond %{HTTP_USER_AGENT} ^$';
+		$sec_rules[] = 'RewriteRule (.*) http://127.0.0.1 [L,R=301]';
+		$sec_rules[] = '</IfModule>';
+	}
+
+	// Insert rules to .htaccess
+	insert_with_markers($htaccess, "SAR One Click Security", $sec_rules);
+
+	// Create .htacces for blocking direct access to PHP files in wp-content/ only if file .htaccess does not exists
+	$wpc_htaccess_exists = file_exists ( $wp_content_htaccess );
+
+	if (!$wpc_htaccess_exists) {
+
+		$wp_content_sec_rules = '# BEGIN SAR One Click Security'.PHP_EOL.'<FilesMatch "\.(php)$">'.PHP_EOL.'order allow,deny'.PHP_EOL.'deny from all'.PHP_EOL.'</FilesMatch>'.PHP_EOL.'# END SAR One Click Security';
+		file_put_contents($wp_content_htaccess, $wp_content_sec_rules, LOCK_EX);
+
+		// Stores an option to be sure that we delete (in the future) a file that we have created
+		add_option( 'sar_ocs_wpc_htaccess', 'yes' );	
+
+	}
+
+}
+
+
+function SAR_Remove_Security_Rules(){
+
+	// Path to .htaccess
+	$htaccess = get_home_path().".htaccess";
+	$wp_content_htaccess = WP_CONTENT_DIR.'/.htaccess';
+
+	$wp_content_htaccess_owned = get_option( 'sar_ocs_wpc_htaccess', true );
+	
+	// Empty rules 
+	$sec_rules = array();
+	
+	// Remove rules. Markers will remain, but are only comments. TODO: Maybe create a new function to remove markers too. 
+	insert_with_markers($htaccess, "SAR One Click Security", $sec_rules);
+
+	if ($wp_content_htaccess_owned === 'yes') {
+
+		// Remove .htacces from wp-content that we have created
+		unlink($wp_content_htaccess);
+		delete_option('sar_ocs_wpcontent_htaccess');
+
+	}
+
+
+}
+
+register_activation_hook( __FILE__, 'SAR_Add_Security_Rules' );
+register_deactivation_hook( __FILE__, 'SAR_Remove_Security_Rules' );
+
+?>
